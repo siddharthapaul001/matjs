a49326f2665308fe80286b0b4ccc9a93b5f02383
siddharthapaul001 <siddharthapaul@live.com>
2020-07-28 21:24:16 +0530

    removing current logics for array handling logic change

--- src/matrix/helper/index.js
+++ src/matrix/helper/index.js
@@ -3,21 +3,7 @@ function deepCopy(obj) {
 }
 
 function matFill(dim, defaultFill) {
-    let mat, dimArr = [...dim], n = dimArr.pop(), newMat;
-    while (n) {
-        if (mat) {
-            newMat = Array(n);
-            newMat[0] = mat;
-            for (let i = 1, l = n; i < l; i++) {
-                newMat[i] = deepCopy(mat);
-            }
-            mat = newMat;
-        } else {
-            mat = Array(n).fill(defaultFill);
-        }
-        n = dimArr.pop();
-    }
-    return mat;
+    
 }
 
 export {
--- src/matrix/index.js
+++ src/matrix/index.js
@@ -1,11 +1,14 @@
 import { matFill, deepCopy } from './helper';
+import dot from '../common/dot';
+import add from '../common/add';
+import substract from '../common/substract';
+import { SCALER_SUBSTRACT } from '../common/lib';
 
 export default class Matrix {
     constructor(arr, defaultFill) {
         this._arr = []; this._dim;
         if (typeof defaultFill !== 'undefined') {
             // expecting arr is a single dimension Array specifing dimension
-            this._arr = matFill(arr, defaultFill);
             this._dim = [...arr];
         } else {
             this.setValues(arr);
@@ -21,15 +24,7 @@ export default class Matrix {
     }
 
     setValues(arr) {
-        let dim = [], _ptr = arr;
-        while(_ptr) {
-            dim.push(_ptr.length);
-            if(Array.isArray(_ptr[0])) {
-                _ptr = _ptr[0];
-            }
-        }
-        this._arr = deepCopy(arr);
-        this._dim = dim;
+
     }
 
     getValues() {
@@ -37,17 +32,14 @@ export default class Matrix {
     }
 
     setValue(dim, value) {
-        if (dim.length === this.dim()) {
-            this.getValue(dim.slice(0, -1))[dim[dim.length - 1]] = value;
-        }
+        
     }
 
     getValue(dimIdx) {
-        let _ptr = this._arr, i = 0;
-        while(_ptr && i < dimIdx.length) {
-            _ptr = _ptr[dimIdx[i]]
-            i++;
-        }
-        return _ptr;
+        
+    }
+
+    copy () {
+        return new Matrix(this._arr);
     }
 }
