1c712b69bb6102af0c0f75e843e3575d7daa2022
siddharthapaul001 <siddharthapaul@live.com>
2020-07-28 23:31:25 +0530

    matrix array handling functions added

--- src/matrix/helper/index.js
+++ src/matrix/helper/index.js
@@ -6,7 +6,63 @@ function matFill(dim, defaultFill) {
     
 }
 
+function countBySize(size) {
+    return size.reduce((count, sub) => {
+        return count * sub;
+    }, 1);
+}
+
+function getSizeByArray(arr) {
+    let ptr = arr, size = [];
+    while (Array.isArray(ptr)) {
+        size.push(ptr.length);
+        ptr = ptr[0];
+    }
+    return size;
+}
+
+function getSingleDimArray (multiDimArr, dim) {
+    let singleDimArr = multiDimArr;
+    if (!dim) {
+        dim = getSizeByArray(multiDimArr).length;
+    }
+    for (let i = 0; i < dim; i++) {
+        singleDimArr = singleDimArr.flat();
+    }
+    return singleDimArr;
+}
+
+function getMultiDimArray (singleDimArr, size) {
+    let tempSize = [...size], n = tempSize.pop(),
+        remDim = tempSize.length,
+        multiDimArr = singleDimArr;
+    while (remDim) {
+        let tempArr = [];
+        for (let i = 0, l = multiDimArr.length; i < l; i += n) {
+            tempArr.push(multiDimArr.slice(i, i + n));
+        }
+        multiDimArr = tempArr;
+        n = tempSize.pop();
+        remDim--;
+    }
+    return multiDimArr;
+}
+
+function sizeToIndex () {
+
+}
+
+function indexToSize () {
+
+}
+
 export {
     deepCopy,
-    matFill
+    matFill,
+    countBySize,
+    getSizeByArray,
+    getSingleDimArray,
+    getMultiDimArray,
+    sizeToIndex,
+    indexToSize
 }
--- src/matrix/index.js
+++ src/matrix/index.js
@@ -1,4 +1,5 @@
-import { matFill, deepCopy } from './helper';
+import { matFill, deepCopy, countBySize, getSizeByArray, getSingleDimArray,
+    getMultiDimArray } from './helper';
 import dot from '../common/dot';
 import add from '../common/add';
 import substract from '../common/substract';
@@ -6,29 +7,38 @@ import { SCALER_SUBSTRACT } from '../common/lib';
 
 export default class Matrix {
     constructor(arr, defaultFill) {
-        this._arr = []; this._dim;
-        if (typeof defaultFill !== 'undefined') {
+        this._arr = []; this._size;
+        if (typeof defaultFill === 'number') {
             // expecting arr is a single dimension Array specifing dimension
-            this._dim = [...arr];
+            this._arr = (new Array(countBySize(arr))).fill(defaultFill);
+            this._size = [...arr];
         } else {
             this.setValues(arr);
         }
     }
 
     size () {
-        return this._dim;
+        return this._size;
     }
 
     dim() {
-        return this._dim.length;
+        return this._size.length;
     }
 
     setValues(arr) {
-
+        let size = getSizeByArray(arr);
+        if (!this._size) {
+            // new matrix
+            this._size = size;
+            this._arr = getSingleDimArray(arr, this.dim());
+        } else {
+            // check for dim matching
+            
+        }
     }
 
     getValues() {
-        return this._arr;
+        return getMultiDimArray(this._arr, this._size);
     }
 
     setValue(dim, value) {
